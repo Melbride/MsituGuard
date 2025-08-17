f7591d7b3debffa300e7516894db2c7436bc910a
Melbride <melbrideb@gmail.com>
2025-08-18 02:36:43 +0300

    Fix build script to use python3 for Vercel compatibility

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Install dependencies
-python -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 # Run migrations
-python manage.py migrate --noinput
+python3 manage.py migrate --noinput
