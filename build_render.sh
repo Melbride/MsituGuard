cc97c728c89fdc3548c1ab38be12edd1c8234a5d
Melbride <melbrideb@gmail.com>
2025-08-18 03:52:40 +0300

    Make migrations optional during build

--- build_render.sh
+++ build_render.sh
@@ -2,5 +2,7 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py migrate
 python manage.py collectstatic --noinput
+
+# Try migrations, but don't fail if database is unreachable
+python manage.py migrate --noinput || echo "Migration failed, will retry at runtime"
